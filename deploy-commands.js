1bb42c6b72f11df4fd468560bae78c2fc1674df5
lasted <shihabmohamed04@gmail.com>
2022-11-16 20:01:06 +0000

    Added a command manager
    Command manager is to only publish certain commands globally once they have been completed so there is no need to publish all at once

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,14 +6,21 @@ const guildId = process.env.GUILDID
 
 
 const commands = [];
+const globalCommands = []
 // Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./test').filter(file => file.endsWith('.js'));
+const globalFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`./test/${file}`);
 	commands.push(command.data.toJSON());
-}
+};
+
+for (const file of globalFiles) {
+	const globalCommand = require(`./commands/${file}`);
+	globalCommands.push(globalCommand.data.toJSON());
+};
 
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
@@ -30,6 +37,21 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 		);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+
+	try {
+		console.log(`Started refreshing ${globalCommands.length} global (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: globalCommands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} global (/) commands.`);
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
 		console.error(error);
