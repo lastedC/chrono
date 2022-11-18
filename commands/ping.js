3af843ed2b264791d69e8ba6b6115016f9a37912
lasted <shihabmohamed04@gmail.com>
2022-11-18 05:58:17 +0000

    Changed desc. and added emojis

--- commands/ping.js
+++ commands/ping.js
@@ -3,12 +3,13 @@ const { SlashCommandBuilder, Discord } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with the bots ping!'),
 	async execute(interaction, client) {
 		try {
 			const mesg = await interaction.reply({ content: "Pong!", fetchReply: true });
 	  
-			await interaction.editReply({ content: `Pong!\nBot Latency: \`${mesg.createdTimestamp - interaction.createdTimestamp}ms\`` });
+			await interaction.editReply({ content: `⌛ Getting ping...` });
+			await interaction.editReply({ content: `🏓 Pong! Bot Latency: \`${mesg.createdTimestamp - interaction.createdTimestamp}ms\`` });
 		  } catch (err) {
 			console.log("Something Went Wrong => ", err);
 		  }
